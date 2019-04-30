1706cf242381444a04f890f2b797e09ff4a5519e
shubhamkr456 <shubhamkr.456@gmail.com>
2019-04-30 10:58:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arduino/ReceveThread.java
@@ -0,0 +1,15 @@
+package com.example.arduino;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Looper;
+
+public class ReceveThread extends Thread {
+   /* //private BluetoothSocket msocket;
+    public ReceiveHandler mReceiveHandler;
+    @Override
+    public void run() {
+        Looper.prepare();
+        mReceiveHandler= new ReceiveHandler();
+        Looper.loop();
+            }*/
+}
